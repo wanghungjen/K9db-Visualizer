6723a144731264798143bd3d54f6ce84e721067d
Hung-Jen Wang <hung-jen_wang@brown.edu>
2023-10-19 19:40:45 -0400

    button?

---
+++ k9db-visualizer/src/components/BoardComponent/ControlPanelComponent/MyControlButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Controls, ControlButton } from "reactflow";
+
+// Props : Name, Action
+
+const MyControlButton = (props) => {
+  const style = {};
+
+  return (
+    <div>
+      <ControlButton onClick={() => props.action} title="action">
+        <div style={{ color: "black" }}>{props.name}</div>
+      </ControlButton>
+    </div>
+  );
+};
+
+export default MyControlButton;
