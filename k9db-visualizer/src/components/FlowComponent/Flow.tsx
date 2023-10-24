ef7d4e905de468f2d07be5c53b6a6b9f85218224
Nora Tang <jingyu_tang@brown.edu>
2023-10-23 20:49:03 -0400

    add node support

--- k9db-visualizer/src/components/FlowComponent/Flow.tsx
+++ k9db-visualizer/src/components/FlowComponent/Flow.tsx
@@ -15,17 +15,22 @@ import OwnsEdge from "./EdgeComponent/OwnsEdge";
 import OwnedByEdge from "./EdgeComponent/OwnedByEdge";
 import AccessesEdge from "./EdgeComponent/AccessesEdge";
 import AccessedByEdge from "./EdgeComponent/AccessedByEdge";
+import DataSubjectNode from "./NodeComponent/DataSubjectNode";
+import NonDataSubjectNode from "./NodeComponent/NonDataSubjectNode";
 
 const edgeTypes = {
   ownsedge: OwnsEdge,
   ownedbyedge: OwnedByEdge,
   accessesedge: AccessesEdge,
-  accessedbyedge: AccessedByEdge
+  accessedbyedge: AccessedByEdge,
 };
 
+const nodeTypes = {
+  datasubjectnode: DataSubjectNode,
+  nondatasubjectnode: NonDataSubjectNode,
+};
 
 const Flow = () => {
-
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
@@ -40,6 +45,7 @@ const Flow = () => {
         nodes={nodes}
         edges={edges}
         edgeTypes={edgeTypes}
+        nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
@@ -48,7 +54,12 @@ const Flow = () => {
         attributionPosition="top-right"
       >
         <ControlPanel />
-        <MiniMap />
+        <MiniMap
+          nodeColor={(n) => {
+            if (n.type === "datasubjectnode") return "#423E37";
+            return "#eee";
+          }}
+        />
         <Background gap={12} size={1} />
       </ReactFlow>
     </div>
