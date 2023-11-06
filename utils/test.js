889d0130f2cd6c0f53cdf74565a0668f5bf65069
Mingchao Zhang <mingchao@Mingchaos-MacBook-Air-4.local>
2023-11-06 14:12:17 -0500

    finished parsing cardinalities of fields

--- utils/test.js
+++ utils/test.js
@@ -1,10 +1,11 @@
 import parse from './parse.js';
 import { getGraph, topoSort } from './graph.js';
 import { calculateCoordinates } from './coordinate.js'
-import {lobstersStatements} from './statements.js'
+import { lobstersStatements, ownCloudStatements } from './statements.js'
 
 // get graph and topologically-sorted nodes
 var parsedObjects = parse(lobstersStatements)
+// var parsedObjects = parse(ownCloudStatements)
 console.log(parsedObjects)
 var graph = getGraph(parsedObjects)
 var sortedNodes = topoSort(graph)
